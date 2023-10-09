4c1512b3f122a12dfd29b879f727c2d3382c6c7b
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-10-09 20:25:45 -0300

    feat: complement projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,18 +3,35 @@ import { projects } from "@/db/projects";
 
 export default function Projects() {
   return (
-    <div className="mt-5">
-      <h1 className="text-[2rem]  uppercase font-semibold">
-        Back-end Projects
-      </h1>
+    <div className="flex flex-col gap-10 mt-5">
+      <section>
+        <h1 className="text-[2rem]  uppercase font-semibold">
+          Back-end Projects
+        </h1>
 
-      <section className="flex gap-5 mt-5">
-        {projects.map(
-          (project) =>
-            project.type === "BACK" && (
-              <ProjectCard key={project.id} project={project} />
-            )
-        )}
+        <div className="flex gap-5 mt-2">
+          {projects.map(
+            (project) =>
+              project.type === "BACK" && (
+                <ProjectCard key={project.id} project={project} />
+              )
+          )}
+        </div>
+      </section>
+
+      <section>
+        <h1 className="text-[2rem]  uppercase font-semibold">
+          Front-end Projects
+        </h1>
+
+        <div className="flex gap-5 mt-5">
+          {projects.map(
+            (project) =>
+              project.type === "FRONT" && (
+                <ProjectCard key={project.id} project={project} />
+              )
+          )}
+        </div>
       </section>
     </div>
   );
